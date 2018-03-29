9599bee8fb3e2e3f08227a11b8713942f8be0b0a
hugo <1099221484@qq.com>
2018-03-30 01:51:41 +0800

    更新请求地址

--- application/route.php
+++ application/route.php
@@ -19,19 +19,23 @@ return [
     ],
     
 	//页面请求
-     '/admin/' =>['admin/View/admin',['method' => 'get']],
-     '/login/' =>['admin/View/login',['method' => 'get']],
+     '/admin' =>['admin/View/admin',['method' => 'get']],
+     '/login' =>['admin/View/login',['method' => 'get']],
      '/admin/welcome' =>['admin/View/welcome',['method' => 'get']],
      '/admin/admin-list' => ['admin/View/user_list',['method' => 'get']],
      '/admin/share-list' => ['admin/View/share_list',['method' => 'get']],
      '/admin/sreach-user' => ['admin/View/sreach_user',['method' => 'get']],
      '/admin/sys-edit' => ['admin/View/sys_edit',['method' => 'get']],
-     
+     '/admin/updeted-list' => ['admin/View/updeted_list',['method' => 'get']],
+     '/admin/updeted-add' => ['admin/View/updeted_add',['method' => 'get']],
+     '/admin/updeted-edit' => ['admin/View/updeted_edit',['method' => 'get']],
 	 
 	 //api请求
 	 '/api/login' => ['admin/Api/login',['method' => 'post']],
 	 '/api/quit' => ['admin/Api/quit',['method' => 'get']],
 	 '/api/sys-edit'=> ['admin/Api/sys_edit',['method' => 'post']],
-	 
+	 '/api/updeted-add'=> ['admin/Api/updeted_add',['method' => 'post']],
+	 '/api/updeted-edit'=> ['admin/Api/updeted_edit',['method' => 'post']],
+	 '/api/upload'=> ['admin/Api/upload',['method' => 'post']],
 
 ];
